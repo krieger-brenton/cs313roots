7f3dd8cad33178f2a9cee5d28836c40b53a918e4
unknown <kreiger.brenton@gmail.comgit config --global user.email Brenton>
2015-02-09 20:51:03 -0700

    Found the bug

--- files_php/week2.php
+++ files_php/week2.php
@@ -21,4 +21,48 @@ print "<div>";
   }
 print "</div>";
 
+$SQL = $db->query("SELECT class_name, save1, save2, hit_die FROM class_info");
+$results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+
+print "<div>";
+  foreach($results as $row)
+  {
+    $line = "Class Name: " . $row['class_name'] . " First Save: " . $row['save1'] . " Second save: " . $row['save2'] . " Hit die: 1" . $row['hit_die'];
+    print $line;
+  }
+print "</div>";
+
+$SQL = $db->query("SELECT property_name, content FROM property");
+$results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+
+print "<div>";
+  foreach($results as $row)
+  {
+    $line = "Property Name: " . $row['property_name'] . ": " . $row['content'];
+    print $line;
+  }
+print "</div>";
+
+$SQL = $db->query("SELECT race_name, size, speed FROM race");
+$results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+
+print "<div>";
+  foreach($results as $row)
+  {
+    $line = "Race: " . $row['race_name'] . " Size: " . $row['size'] . " Speed: " . $row['speed'] . "ft.";
+    print $line;
+  }
+print "</div>";
+
+$SQL = $db->query("SELECT weapon_name, damage, damage_type FROM weapon");
+$results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+
+print "<div>";
+  foreach($results as $row)
+  {
+    $line = "Name: " . $row['weapon_name'] . " Damage: " . $row['damage'] . " Damage Type: " . $row['damage_type'];
+    print $line;
+  }
+print "</div>";
+
 ?>
